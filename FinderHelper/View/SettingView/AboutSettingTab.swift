3363d18dab40e145a16ff862a5f32659e5a44ae6
kyle <kyle201817146@gmail.com>
2021-11-03 11:26:19 +0800

    Update About page

--- FinderHelper/View/SettingView/AboutSettingTab.swift
+++ FinderHelper/View/SettingView/AboutSettingTab.swift
@@ -12,14 +12,19 @@ struct AboutSettingTab: View {
     private let contentWidth: Double = 450.0
 
     var body: some View {
-        HStack {
-            Text("Made by")
-            Image("Kyle")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 30, height: 30)
-                .clipShape(Circle())
-            Text("with 🥰")
+        VStack {
+            HStack {
+                Text("Made by")
+                Image("Kyle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30, height: 30)
+                    .clipShape(Circle())
+                Text("[Kyle Ye](https://github.com/Kyle-Ye)")
+                Text("with 🥰")
+            }
+            Text("Inspired by [SwiftyMenu](https://apps.apple.com/cn/app/swiftymenu/id1567748223?l=en&mt=12) [Shengang Tang](https://twitter.com/lexrus)")
+                .font(.footnote)
         }
         .font(.title)
         .frame(width: CGFloat(contentWidth), alignment: .center)
