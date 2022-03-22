2927c83503477e6a578b18773e79b9797d0e3df1
Kyle <kyle201817146@gmail.com>
2022-03-23 02:00:12 +0800

    Add format hint

--- MenuHelper/View/Editor/AppMenuItemEditor.swift
+++ MenuHelper/View/Editor/AppMenuItemEditor.swift
@@ -41,41 +41,49 @@ struct AppMenuItemEditor: View {
                 Text("Display Name:")
                 TextField("Display Name", text: $item.itemName)
             }
-            HStack {
-                Text("Arguments:")
-                TextField("Arguments", text: $argumentString)
+            VStack {
+                HStack {
+                    Text("Arguments:")
+                    TextField("Arguments", text: $argumentString)
+                }
+                Text("Format: \("-a -b --help")").font(.footnote).foregroundColor(.secondary)
             }
-            HStack {
-                Text("Environment:")
-                TextField("Environment", text: $environmentString)
+            VStack {
+                HStack {
+                    Text("Environment:")
+                    TextField("Environment", text: $environmentString)
+                }
+                Text("Format: \("KEY_A=0 KEY_B=1")").font(.footnote).foregroundColor(.secondary)
             }
             Spacer()
         }
         .padding()
-        .frame(width: 400, height: 300)
         .toolbar {
-            Button {
-                dismiss()
-            } label: {
-                Image(systemName: "xmark.circle")
-            }
-            Spacer()
-            Button {
-                Task {
-                    item.arguments = argumentString.split(separator: " ").map { String($0) }
-                    item.environment = environmentString.split(separator: " ")
-                        .map { $0.split(separator: "=") }
-                        .filter { $0.count == 2 }
-                        .reduce(into: [String: String]()) { result, pair in
-                            let key = String(pair[0])
-                            let value = String(pair[1])
-                            result[key] = value
-                        }
-                    store.updateAppItem(item: item, index: index)
+            ToolbarItem(placement: .cancellationAction) {
+                Button {
                     dismiss()
+                } label: {
+                    Image(systemName: "xmark.circle")
                 }
-            } label: {
-                Image(systemName: "checkmark.circle")
+            }
+            ToolbarItem(placement: .confirmationAction) {
+                Button {
+                    Task {
+                        item.arguments = argumentString.split(separator: " ").map { String($0) }
+                        item.environment = environmentString.split(separator: " ")
+                            .map { $0.split(separator: "=") }
+                            .filter { $0.count == 2 }
+                            .reduce(into: [String: String]()) { result, pair in
+                                let key = String(pair[0])
+                                let value = String(pair[1])
+                                result[key] = value
+                            }
+                        store.updateAppItem(item: item, index: index)
+                        dismiss()
+                    }
+                } label: {
+                    Image(systemName: "checkmark.circle")
+                }
             }
         }
         .onAppear {
